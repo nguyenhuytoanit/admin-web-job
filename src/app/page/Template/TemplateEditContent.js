a632d4ce8b6dc5e3ae0be9f28dfee84bcc2bef62
Huy Toan <nguyenhuytoanit@gmail.com>
2022-10-24 19:43:35 +0700

    master

--- src/app/page/Template/TemplateEditContent.js
+++ src/app/page/Template/TemplateEditContent.js
@@ -1,15 +1,11 @@
 import { Input } from "app/common/forms/Input";
+import { Select } from "app/common/forms/Select";
 import { Field, FieldArray, Form, Formik } from "formik";
 import React from "react";
 
 function TemplateEditContent(props) {
   const initialValues = {
-    friends: [
-      {
-        name: "",
-        email: "",
-      },
-    ],
+    friends: [],
   };
 
   return (
@@ -25,6 +21,101 @@ function TemplateEditContent(props) {
         >
           {({ values }) => (
             <Form>
+              <div className="form-group row">
+                <div className="col-lg-12 mt-2">
+                  <Field
+                    name="name"
+                    component={Input}
+                    placeholder={"Nhập tên block"}
+                    customFeedbackLabel
+                    withFeedbackLabel
+                    focus
+                  />
+                </div>
+              </div>
+              <FieldArray name="friends">
+                {({ insert, remove, push }) => (
+                  <div>
+                    {values.friends.length > 0 &&
+                      values.friends.map((friend, index) => (
+                        <div className="card mb-3" key={index}>
+                          <div className="row m-2">
+                            <div className="col-lg-8">
+                              <Field
+                                name={`friends.${index}.name`}
+                                component={Input}
+                                placeholder={"Nhập nội dung đoạn văn"}
+                                customFeedbackLabel
+                                withFeedbackLabel
+                                focus
+                              />
+                            </div>
+                            <div className="col-lg-4">sdf</div>
+                            <Select
+                              name="construction"
+                              label={"Thuộc công trình"}
+                              customFeedbackLabel
+                              withFeedbackLabel
+                            >
+                              <option value="" hidden>
+                                Chọn công trình trực thuộc
+                              </option>
+                              <option value={"1"}>Công trình</option>
+                              <option value={"2"}>Công trình 2</option>
+                            </Select>
+                            {/* <div className="col">
+                              <button
+                                type="button"
+                                className="secondary"
+                                onClick={() => remove(index)}
+                              >
+                                X
+                              </button>
+                            </div> */}
+                          </div>
+                        </div>
+                      ))}
+                    <div
+                      className=" d-flex align-items-center justify-content-center mt-3 text-primary cursor-pointer"
+                      onClick={() => push({ name: "", email: "" })}
+                    >
+                      <div className="d-inline-block">
+                        <i class="fa-solid fa-plus"></i>
+                        <span> Thêm mới nội dung</span>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </FieldArray>
+              {/* <button type="submit">Invite</button> */}
+            </Form>
+          )}
+        </Formik>
+      </div>
+
+      <div class="p-3 mb-2 bg-secondary text-white">Kiểm tra</div>
+      <div className="card p-3 my-3">
+        <Formik
+          initialValues={initialValues}
+          onSubmit={async (values) => {
+            await new Promise((r) => setTimeout(r, 500));
+            alert(JSON.stringify(values, null, 2));
+          }}
+        >
+          {({ values }) => (
+            <Form>
+              <div className="form-group row">
+                <div className="col-lg-12 mt-2">
+                  <Field
+                    name="name"
+                    component={Input}
+                    placeholder={"Nhập tên block"}
+                    customFeedbackLabel
+                    withFeedbackLabel
+                    focus
+                  />
+                </div>
+              </div>
               <FieldArray name="friends">
                 {({ insert, remove, push }) => (
                   <div>
@@ -53,23 +144,23 @@ function TemplateEditContent(props) {
                           </div>
                         </div>
                       ))}
-                    <button
-                      type="button"
-                      className="secondary"
+                    <div
+                      className=" d-flex align-items-center justify-content-center mt-3 text-primary cursor-pointer"
                       onClick={() => push({ name: "", email: "" })}
                     >
-                      Add Friend
-                    </button>
+                      <div className="d-inline-block">
+                        <i class="fa-solid fa-plus"></i>
+                        <span> Thêm mới nội dung</span>
+                      </div>
+                    </div>
                   </div>
                 )}
               </FieldArray>
-              <button type="submit">Invite</button>
+              {/* <button type="submit">Invite</button> */}
             </Form>
           )}
         </Formik>
       </div>
-
-      <div class="p-3 mb-2 bg-secondary text-white">Kiểm tra</div>
     </div>
   );
 }
